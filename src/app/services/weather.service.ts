bb07d2b74d6dfa378b073ecaba0b92ca90cae20f
bardhoshimajlinda <116483572+bardhoshimajlinda@users.noreply.github.com>
2024-02-14 21:28:59 +0100

    implemented weather search component and weather service

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  private apiKey = '63e30647fbe51d36bac1cf4b642ae9c3';
+  private apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+  constructor(private http: HttpClient) { }
+
+  getWeather(city: string): Observable<any> {
+    const url = `${this.apiUrl}?q=${city}&appid=${this.apiKey}`;
+    return this.http.get<any>(url);
+  }
+}
